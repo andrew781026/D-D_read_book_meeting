bcd82e4735553f72674da8a7c38eb3aa147b1fa1
Cai Ci Jhan <ordinarycas@gmail.com>
2021-01-26 03:12:53 +0800

    Add users query

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const usersController = require('../controllers/index').users;
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  usersController.list(req,res)
 });
-
 module.exports = router;
