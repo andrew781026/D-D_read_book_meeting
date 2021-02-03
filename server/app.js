3a980cea62a1265cd8f708277f57d22f6ac03ee8
Cai Ci Jhan <ordinarycas@gmail.com>
2021-02-03 18:15:38 +0800

    Delete test sample file

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const basicAuth = require('express-basic-auth');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var categoryRouter = require('./routes/category');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/category', categoryRouter);
 
 const options = {
   swaggerDefinition: {
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const usersController = require('../controllers/index').users;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  usersController.list(req,res)
 });
 module.exports = router;
