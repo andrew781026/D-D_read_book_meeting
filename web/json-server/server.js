4a73c0e3dc9ce1cb8e9af4a1d11991b564c5dfa9
王澍(Andrew) <andrew7810262000@gmail.com>
2021-02-17 16:00:43 +0800

    [web] 新增 NewCard 物件到 ListContent.js 中

--- web/json-server/server.js
+++ web/json-server/server.js
@@ -1,7 +1,8 @@
 // server.js
 const jsonServer = require('json-server')
+const path = require('path')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
 const PORT = 3003
 
