1d9a88b0d818de7017121e7b64a023e4fbdd257f
Lu406456 <44430613+h406456@users.noreply.github.com>
2021-02-22 19:29:20 +0800

    [ios] 建立 create_event_2 的 UI

---
+++ ios/Study Group App/Study Group App/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Study Group App
+//
+//  Created by Tiff Yang on 2021/1/19.
+//  Copyright © 2021 Tiffany Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
