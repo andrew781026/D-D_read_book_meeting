36f0dbbe3894e8a26a9dc9891ddac0d434f4dcdc
Tiffany Yang <s10050242@gmail.com>
2021-03-08 15:55:45 +0800

    Third commit

---
+++ ios/Study Group App/Study Group App/Controller/RegisterPageController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterPageController.swift
+//  Study Group App
+//
+//  Created by Tiff Yang on 2021/3/8.
+//  Copyright © 2021 Tiffany Yang. All rights reserved.
+//
+
+import UIKit
+import UIKit
+
+class RegisterPagePageController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+       
+    }
+}
