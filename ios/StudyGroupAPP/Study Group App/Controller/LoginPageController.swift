68cf5407bc038a1dee3d708d97e7f36ec0a1e70e
Lu406456 <44430613+h406456@users.noreply.github.com>
2021-03-17 09:36:39 +0800

    update rename

---
+++ ios/StudyGroupAPP/Study Group App/Controller/LoginPageController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageController.swift
+//  Study Group App
+//
+//  Created by Tiff Yang on 2021/3/8.
+//  Copyright © 2021 Tiffany Yang. All rights reserved.
+//
+
+import UIKit
+
+class LoginPageViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+       
+    }
+}
