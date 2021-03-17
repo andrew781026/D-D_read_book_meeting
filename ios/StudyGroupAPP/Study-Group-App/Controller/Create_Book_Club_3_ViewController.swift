5cdca171d2a6fcecb30fd12054445f817ea6b52b
Lu406456 <44430613+h406456@users.noreply.github.com>
2021-03-17 09:51:14 +0800

    update rename  StudyGroupAPP file

---
+++ ios/StudyGroupAPP/Study-Group-App/Controller/Create_Book_Club_3_ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Create_Book_Club_3_ViewController.swift
+//  Study-Group-App
+//
+//  Created by lu on 2021/3/5.
+//
+
+import UIKit
+
+class Create_Book_Club_3_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ios/StudyGroupAPP/Study-Group-App/View/Controller/GroupPageCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  GroupPageCollectionViewCell.swift
+//  Study Group App
+//
+//  Created by Tiff Yang on 2021/3/9.
+//  Copyright © 2021 Tiffany Yang. All rights reserved.
+//
+
+import UIKit
+
+class GroupPageCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
